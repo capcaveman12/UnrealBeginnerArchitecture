d85eb226b75be8ad48cfe46fe0a0772464dfcd4f
adam Mcgee <118749959+capcaveman12@users.noreply.github.com>
2026-04-30 11:05:04 -0500

    Created prototype level

---
+++ Source/UnrealBeginnerArchit/UnrealBeginnerArchit.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealBeginnerArchit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealBeginnerArchit, "UnrealBeginnerArchit" );
